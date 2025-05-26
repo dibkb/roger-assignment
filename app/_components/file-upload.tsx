b8287089d53ce7cbe73c6866907b9843849caf12
dibkb <dibas9110@gmail.com>
2025-05-26 12:55:07 +0530

    file icon change

--- app/_components/file-upload.tsx
+++ app/_components/file-upload.tsx
@@ -4,7 +4,7 @@ import { Card, CardContent, CardFooter } from "@/components/ui/card";
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import { FileIcon, Loader2 } from "lucide-react";
+import { File, FileIcon, Loader2 } from "lucide-react";
 import { useCallback, useState } from "react";
 import { uploadResponseSchema, errorResponseSchema } from "@/lib/zod/api/csv";
 import { useCSVStore } from "@/lib/store/csv-store";
@@ -60,7 +60,7 @@ export default function FileUpload() {
       >
         <CardContent className="p-6 space-y-4">
           <div className="border-2 border-dashed border-gray-200 rounded-lg flex flex-col gap-1 p-6 items-center">
-            <FileIcon className="w-12 h-12" />
+            <File className="my-4 text-neutral-400 size-6" />
             <span className="text-sm font-medium text-gray-500">
               Drag and drop a file or click to browse
             </span>
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import FileUpload from "./_components/file-upload";
 
 export default function Home() {
   return (
-    <div className="flex justify-center items-center container mx-auto h-screen">
+    <div className="flex justify-center items-center container mx-auto h-[calc(100vh-30vh)]">
       <FileUpload />
     </div>
   );
